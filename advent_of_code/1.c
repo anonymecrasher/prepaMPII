bdd3ac885ad5a2437e1a0ec83be0b25a821984f3
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2024-12-06 15:13:34 +0100

    Update 1.c

---
+++ advent_of_code/1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h> 
+
+char tableau[9000000];
+char temptab[13];
+void get_entre()
+{
+  char current_line[1000];
+  while (current_line != 'o')
+    {
+      scanf("%s", current_line);
+    }
+}
+
+
+void main()
+{
+  get_entre();
+}
