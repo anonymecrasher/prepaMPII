5b64b542a0ab98e6070d711062cee3d868b0a9bc
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2025-01-31 15:10:03 +0100

    Add files via upload

--- projet_infix_postfix/pile.c
+++ projet_infix_postfix/pile.c
@@ -9,10 +9,10 @@ struct stack_s {
   int *values;
   int hd; // hd -> stack head
   int max;
-}
+};
 
 stack_int *init(void){
-  stack_int s = malloc(sizeof(stack_int));
+  stack_int *s = malloc(sizeof(stack_int));
   s->values = malloc(init_size * sizeof(int));
   s->hd = -1;
   s->max = init_size;
@@ -28,12 +28,73 @@ int top(stack_int *s){
   return s->values[s->hd];
 }
 void realoc(stack_int *s){
-  tmp = malloc(sizeof(int) * 10 * s->max);
+  int *tmp = malloc(sizeof(int) * (10 * s->max));
   for (int i=0; i < s->hd; i = i + 1){
     tmp[i] = s->values[i];
     }
+  free(s->values);
+  s->values = tmp;
+  s->max = s->max * 10;
 }
 
+void push(int x, stack_int *s){
+  if (s->hd + 1 >= s->max){realoc(s);}
+  s->values = x;
+  s->hd = s->hd + 1;
+}
+
+int pop(stack_int *s){
+  int x = top(s);
+  s->hd = s->hd - 1;
+  return x;
+}
+
+
+struct stack_string {
+  int **values;
+  int hd; // hd -> stack head
+  int max;
+};
+
+
+
+stack_str *init_str(void){
+  stack_str *s = malloc(sizeof(stack_int));
+  s->values = malloc(init_size * sizeof(char*));
+  s->hd = -1;
+  s->max = init_size;
+  return s;
+}
+
+bool is_empty_str(stack_str *s){
+  return (s->hd == -1);
+}
+
+char* top_str(stack_str *s){
+  assert(is_empty(s));
+  return s->values[s->hd];
+}
+void realoc_str(stack_str *s){
+  char* *tmp = malloc(sizeof(char*) * (10 * s->max));
+  for (int i=0; i < s->hd; i = i + 1){
+    tmp[i] = s->values[i];
+    }
+  free(s->values);
+  s->values = tmp;
+  s->max = s->max * 10;
+}
+
+void push_str(char* x, stack_str *s){
+  if (s->hd + 1 >= s->max){realoc(s);}
+  s->values = x;
+  s->hd = s->hd + 1;
+}
+
+char* pop_str(stack_str *s){
+  char* x = top(s);
+  s->hd = s->hd - 1;
+  return x;
+}
 
 
 
--- projet_infix_postfix/pile.h
+++ projet_infix_postfix/pile.h
@@ -6,6 +6,7 @@
 #include <assert.h>
 
 typedef struct stack_s stack_int;
+typedef struct stack_string stack_str; 
 
 stack_int *init(void);
 bool is_empty(stack_int *s);
@@ -13,5 +14,13 @@ int top(stack_int *s);
 void realoc(stack_int *s);
 void push(int x, stack_int *s);
 int pop(stack_int *s);
+
+stack_str *init_str(void);
+bool is_empty_str(stack_str *s);
+char* top_str(stack_str *s);
+void realoc_str(stack_str *s);
+void push_str(char* x, stack_str *s);
+char* pop_str(stack_str *s);
+
 
 #endif
